27fe2c0d8c4562e39cf521d98081ebc7f661d2af
M47781 <146139961+M47781@users.noreply.github.com>
2026-01-28 21:18:52 +0100

    Fix base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/mama-astuce/', // هذا هو السطر اللي زدناه باش تتحل مشكلة الصفحة البيضاء
+      base:,'/.' :base // هذا هو السطر اللي زدناه باش تتحل مشكلة الصفحة البيضاء
       server: {
         port: 3000,
         host: '0.0.0.0',
