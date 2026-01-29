c80e8fc21a74f3416ac1949537a013b76c9accfe
M47781 <146139961+M47781@users.noreply.github.com>
2026-01-29 11:15:26 +0100

    ewfefw

--- components/OrderForm.tsx
+++ components/OrderForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Send, CheckCircle, MapPin, Phone, User, Package } from 'lucide-react';
 
-const productSizes = ['500mg', '1g'];
+const productSizes = ['50g', '1g'];
 
 const algerianWilayas = [
   "01 - أدرار", "02 - الشلف", "03 - الأغواط", "04 - أم البواقي", "05 - باتنة", 
@@ -26,7 +26,7 @@ const OrderForm: React.FC = () => {
     phone: '',
     state: '',
     address: '',
-    productSize: '500mg',
+    productSize: '50g',
   });
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -65,7 +65,7 @@ const OrderForm: React.FC = () => {
   };
 
   const resetForm = () => {
-    setFormData({ fullName: '', phone: '', state: '', address: '', productSize: '500mg' });
+    setFormData({ fullName: '', phone: '', state: '', address: '', productSize: '50g' });
     setSubmitted(false);
   };
 
